8cf268f318c4a849dc39f5fdff385b3a111ca68f
SHANMUKHAVYSHNAVIKARANAM <shanmukhavyshnavi6@gmail.com>
2020-08-31 19:08:58 +0530

    javascript

---
+++ week2/scripts/main.js
@@ -0,0 +1,6 @@
+const myHeading = document.querySelector('h1');
+myHeading.textContent = 'JAVASCRIPT';
+function myfunction()
+{
+	document.getElementById("demo").innerHTML="JavaScript itself is relatively compact, yet very flexible. Developers have written a variety of tools on top of the core JavaScript language, unlocking a vast amount of functionality with minimum effort."
+}
